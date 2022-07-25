9ce0c0770ad844d2df8ad4db99f94bbf65074783
Matt Russelli <russellimatt@gmail.com>
2022-07-25 19:14:04 -0400

    added in full CRUD to card and added in search for the set

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const Card = require('../models/card')
+const Set = require('../models/set')
 
+//card controllers
 const getCard = async (req, res) => {
   try {
     const cards = await Card.find()
@@ -34,8 +36,61 @@ const getCardById = async (req, res) => {
   }
 }
 
+const deleteCard = async (req, res) => {
+  try {
+    const { id } = req.params
+    const cardDelete = await Card.findByIdAndDelete(id)
+    if (cardDelete) {
+      return res.status(200).json({ cardDelete })
+    }
+    throw new Error('Card not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const updateCard = async (req, res) => {
+  try {
+    const { text } = req.body
+    const cardUpdate = await Card.findByIdAndUpdate(req.params.Id, { text })
+    if (!cardUpdate) {
+      res.status(500).send('Card not found')
+    }
+    return res.status(200).json({ cardUpdate })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+//set controlellers
+const getSet = async (req, res) => {
+  try {
+    const sets = await Set.find()
+    return res.status(200).json({ sets })
+  } catch (e) {
+    return res.status(500).send(e.message)
+  }
+}
+
+const getSetById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const set = await Set.findById(id)
+    if (set) {
+      return res.status(200).json({ set })
+    }
+    return res.status(404).send('Card with the specified ID does not exist')
+  } catch (e) {
+    return res.status(500).send(e.message)
+  }
+}
+
 module.exports = {
   getCard,
   createCard,
-  getCardById
+  getCardById,
+  deleteCard,
+  updateCard,
+  getSet,
+  getSetById
 }
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,20 @@ const router = Router()
 
 router.get('/', (req, res) => res.send('Yu-Gi-Ooooooooooooooh!'))
 
+//routes for card
 router.get('/card', controllers.getCard)
 
 router.post('/card', controllers.createCard)
 
 router.get('/card/:id', controllers.getCardById)
+
+router.delete('/card-delete/:id', controllers.deleteCard)
+
+router.put('/card-update/:id', controllers.updateCard)
+
+//route for set
+router.get('/set', controllers.getSet)
+
+router.get('/set/:id', controllers.getSetById)
 
 module.exports = router

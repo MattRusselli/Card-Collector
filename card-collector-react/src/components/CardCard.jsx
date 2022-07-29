a4c12b8603ccfa05e5f47fc1d7b884cafff287cc
Matt Russelli <russellimatt@gmail.com>
2022-07-28 22:18:23 -0400

    some styling

--- card-collector-react/src/components/CardCard.jsx
+++ card-collector-react/src/components/CardCard.jsx
@@ -1,11 +1,15 @@
 const CardCard = (props) => {
   return (
-    <div className="card" onClick={props.onClick}>
-      <img src={props.img} alt={props.name} />
-      <h3 className="description">Name: {props.name} </h3>
-      <h3 className="description">Type: {props.type}</h3>
-      <h3 className="description">Effect: {props.effect}</h3>
-      <h3 className="description">Rarity in Set: {props.rarity}</h3>
+    <div className="cardDetails" onClick={props.onClick}>
+      <div className="cardImg">
+        <img src={props.img} alt={props.name} />
+      </div>
+      <div className="description">
+        <h3>Name: {props.name} </h3>
+        <h3>Type: {props.type}</h3>
+        <h3>Effect: {props.effect}</h3>
+        <h3>Rarity in Set: {props.rarity}</h3>
+      </div>
     </div>
   )
 }
